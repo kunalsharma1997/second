141d876d5a084e58fd443d8db3b8590fa6898699
kunalsharma1997 <58587883+kunalsharma1997@users.noreply.github.com>
2019-12-08 22:21:49 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+struct Box
+{
+  int length;
+  int breadth;
+  int height;
+};
+
+int main() {
+  struct Box b1 = { 10, 12, 5 };
+  struct Box* ptr;
+  //ptr=&b1;
+  ++ptr->height;
+  int volume = ptr->length * ptr->breadth * ptr->height;
+  print("%d,%d,%d\n",ptr->length, ptr->breadth, ptr->height);
+  return 0;
+}
