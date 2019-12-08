3d06905cbaa2375f45879361d265f92a1cbd6f0c
kunalsharma1997 <58587883+kunalsharma1997@users.noreply.github.com>
2019-12-08 22:34:05 +0530

    Update main.c

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ struct Box
 int main() {
   struct Box b1 = { 10, 12, 5 };
   struct Box* ptr;
-  //ptr=&b1;
+  ptr=&b1;
   ++ptr->height;
   int volume = ptr->length * ptr->breadth * ptr->height;
   print("%d,%d,%d\n",ptr->length, ptr->breadth, ptr->height);
